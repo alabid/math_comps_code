69c018438f4d5835159b49e8f541bc13bedeadb8
Daniel Alabi <alabid@carleton.edu>
2014-05-07 17:05:27 -0500

    first commit?

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-math_thesis_code
-================
+Code for my Math Thesis Presentation
+=====================================
+
+This repository contains implementations for some algebraic
+and combinatorial methods of solving the Maximum Matching
+problem in graphs.
+
+Algebraic Implementations
+-------------------------
+1. Rabin-Vazirani Algorithm
+
+Combinatorial Implementations
+-----------------------------
+1. Blossom Algorithm
+
---
+++ graph.py
@@ -0,0 +1,24 @@
+'''
+By Daniel Alabi.
+Adjacency matrix implementation of a graph. 
+'''
+
+import numpy
+
+class Graph:
+    def __init__(self, n):
+        self.num_vertices = n
+        self.matrix = [[0]*n for i in range(n)]
+        
+    def add_edge(self, u, v):
+        self.matrix[u][v] = 1
+        self.matrix[v][u] = 1
+
+    def determinant(self):
+        return numpy.linalg.det(self.matrix)
+
+g = Graph(3)
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 2)
+print g.determinant()
